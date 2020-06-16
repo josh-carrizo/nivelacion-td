f629ae5e9efdd7d6127e53e445c13e3bd04d79a8
unknown <sistemas@VRE_NB_CAP1804.inacap.cl>
2020-06-16 16:42:12 -0400

    first commit

---
+++ nivelacion-td-covid-master/js/script-index.js
@@ -0,0 +1,74 @@
+$(document).ready( function(){
+
+	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
+	renderHighlightedRecipes(recipesArray);
+
+});
+
+
+/*
+* Función que se encarga de pintar TODAS las recetas que tengan 
+* marcado el atributo "highlighted" como TRUE
+*/
+function renderHighlightedRecipes(recipesArray) {
+	console.log('Recipes: ', recipesArray);
+/*	for (var i=0; i<recipesArray.length; i++){
+		if(recipesArray[i].highlighted == true){
+			renderRecipe(recipesArray[i]);
+		}
+}*/
+	recipesArray.forEach( function(recipe){
+		if(recipesArray[i].highlighted == true){
+			renderRecipe(recipe);
+		
+	});
+}
+
+/*
+* Función que se encarga de pintar UNA recetas que tenga 
+* marcado el atributo "highlighted" como TRUE
+* Aqui se tiene que crear el HTML que esta en el 
+* archivo "templates/templates-recipe.html"
+*/
+function renderRecipe(recipe) {
+	
+	$('.list-recipes').append(
+	`<a class="item-recipe" href="#">
+		<span class="attribution">
+	 	<span class="title-recipe">${recipe.title}</span>
+	  	<span class="metadata-recipe">
+			<span class="author-recipe">${recipe.source.name} </span>
+			<span class="bookmarks-recipe">
+		  	<span class="icon-bookmark"></span> 
+		</span>
+	  </span>
+	</span>
+  
+	<img src="img/recipes/320x350/${recipe.name}.jpg" />
+  </a>`
+  )
+
+}
+
+
+
+/*
+* Función que se encarga de pintar todas las actividades
+*/
+function renderActivities(activitiesArray) {
+	if(activitiesArray.length > 0){
+		$('wrapper-message')
+	}
+	//console.log('Activities: ', activitiesArray);
+}
+
+/*
+* Función que se encarga de pintar una actividad
+* Aqui se tiene que crear el HTML que esta en el 
+* archivo "templates/templates-activity.html"
+*/
+function renderActivity(recipe) {
+	
+}
+
+
